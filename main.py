dbd6a3a111ce0408c34d6583397171626a454efa
armeen28 <armeen.hasan@laetottenham.ac.uk>
2024-11-01 20:20:47 +0000

    main

--- main.py
+++ main.py
@@ -7,24 +7,29 @@ cat_attributes = {
 print("Welcome to my cat game!")
 
 name = input("Enter name of your new cat:")
-colour = input("Enter colour of your new cat: )
+colour = input("Enter colour of your new cat: ")
 
-# start the while loop
+
 while True:
-    # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
+    
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. Show stats 4. Feed your cat")
 
     if option == '1':
-        cat_attributes[energy] -= 1 
+        cat_attributes['energy'] -= 1 
         pass
     elif option == '2':
-        cat_attributes[intelligence] += 1
+        cat_attributes['intelligence'] += 1  
+        cat_attributes['energy'] -= 1
         pass
     
     elif option == '3':
         print("Your stats: ")
         for x in cat_attributes:
             print(f"Your cat's {x} in {cat_attributes[x]}")
+    
+    elif option == '4':
+        cat_attributes['weight'] -= 1
+        pass
 
     else:
     
@@ -34,5 +39,8 @@ while True:
     if cat_attributes['energy'] < 0:
         print("Oh no! You just killed your cat")
         pass
-    # elif ...
+    elif cat_attributes['energy'] > 0:
+        print("Keep going! Give your cat more energy")
+
+
     
